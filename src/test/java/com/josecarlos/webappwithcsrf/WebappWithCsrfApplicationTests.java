4294ee1463638a25f101abc85899334ed7ec2bab
Juan Carlos Ramirez <jccortavee@gmail.com>
2022-06-20 20:45:49 -0600

    Working with CSRF and Thymeleaf

---
+++ src/test/java/com/josecarlos/webappwithcsrf/WebappWithCsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josecarlos.webappwithcsrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappWithCsrfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
